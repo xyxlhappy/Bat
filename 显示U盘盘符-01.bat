30752b3b15c1527109998656089d8c2569d29d08
xyxlhappy <77725612+xyxlhappy@users.noreply.github.com>
2021-06-09 16:20:59 +0800

    Delete 显示U盘盘符-01.bat

--- 显示U盘盘符-01.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-for %%a in (c d e f g h i j k l m n o p q r s t u v w x y z) do (
-for /f %%h in ('fsutil fsinfo drivetype %%a:^|findstr "Removable.* 可移动"') do (
-set UPan=%%h
-)
-)
-echo.%UPan%
-%UPan%
-pause
